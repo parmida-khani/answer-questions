3981855c8273c5716899814ef018d659b85aa36c
Parmida Khani <khani1379@yahoo.com>
2023-08-12 21:59:06 +0330

    change navbar header based on url

--- components/CreateDialog.tsx
+++ components/CreateDialog.tsx
@@ -1,15 +1,26 @@
-'use client'
 import CreateProblem from "@/components/CreateProblem";
 import * as React from "react";
 import {Button} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 
 export default function CreateDialog() {
     const [open, setOpen] = React.useState(false);
 
     return (
         <>
-            <Button variant="contained" sx={{ml: 4, color: "white", backgroundColor: '#27AE60 !important'}}
-                    onClick={() => setOpen(true)}>
+            <Button
+                variant="contained"
+                sx={{
+                    ml: 2,
+                    color: "white",
+                    backgroundColor: '#27AE60 !important',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+                onClick={() => setOpen(true)}
+            >
+                <AddIcon sx={{ml: 1, fontSize: '1rem'}}/>
                 سوال جدید
             </Button>
             <CreateProblem open={open} handleClose={() => setOpen(false)}/>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -6,31 +7,40 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import CreateDialog from '@/components/CreateDialog';
+import {usePathname} from 'next/navigation'
+import {Hidden} from "@mui/material";
 
 export default function Navbar() {
+    const pathname = usePathname();
+    const pageTitle = pathname.includes('details') ? 'جزییات سوال' : 'لیست سوالات';
+
     const auth = true;
+    const username = 'پارمیدا خانی';
+
+    const userInfo = <>
+        <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+        >
+            <AccountCircle/>
+        </IconButton>
+        <Hidden smDown>
+            <Typography variant="body2">{username}</Typography>
+        </Hidden>
+    </>
 
     return (
-        <Box sx={{ flexGrow: 1, mb: 4 }}>
-            <AppBar position="static" sx={{ backgroundColor: 'white', boxShadow: '0 0 5px lightgray' }}>
-                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Typography variant="h6" component="div" sx={{ color: 'black', fontWeight: 'bold' }}>
-                        لیست سوالات
+        <Box sx={{flexGrow: 1, mb: 4}}>
+            <AppBar position="static" sx={{backgroundColor: 'white', boxShadow: '0 0 5px lightgray'}}>
+                <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
+                    <Typography variant="h6" component="div" sx={{color: 'black', fontWeight: 'bold'}}>
+                        {pageTitle}
                     </Typography>
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <CreateDialog />
-                        {auth && (
-                            <>
-                                <IconButton
-                                    size="large"
-                                    aria-label="account of current user"
-                                    aria-controls="menu-appbar"
-                                    aria-haspopup="true"
-                                >
-                                    <AccountCircle />
-                                </IconButton>
-                            </>
-                        )}
+                    <div style={{display: 'flex', alignItems: 'center'}}>
+                        <CreateDialog/>
+                        {auth && userInfo}
                     </div>
                 </Toolbar>
             </AppBar>
