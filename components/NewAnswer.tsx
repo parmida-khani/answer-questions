a49c5cf73f8798c91887c0fc8b88c8a56cee80ab
Parmida Khani <khani1379@yahoo.com>
2023-08-13 01:41:37 +0330

    parse id string to int

--- components/NewAnswer.tsx
+++ components/NewAnswer.tsx
@@ -11,7 +11,7 @@ export default function NewAnswer({totalAnswers}: { totalAnswers: number }) {
     const [isEmpty, setIsEmpty] = useState(false);
     const [input, setInput] = useState<string>('');
     const newTotalAnswers = useRef<number>(totalAnswers + 1);
-    const {id: problemId} = useParams();
+    const problemId: string = useParams().id;
 
     const queryClient = useQueryClient();
     const createAnswerMutation = useMutation({
